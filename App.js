1ab042ca19aa4888405a2c7e989d3d67b9c009e0
irvin1305 <irvingonzalez00213@gmail.com>
2026-05-20 14:34:39 -0500

    funcionamiento listo (apurate charlotte)

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text,View,TouchableOpacity,TextInput} from 'react-native';
 
 export default function compraAuto() {
   const[costo,setCosto]= useState('');
+  const [salario, setSalario] =useState('')
   const[transmision, setTransmision]= useState('manual');
+  const [formaPago, setFormaPago] = useState('contado');
 
   //Calculos de manual y automatico
 function calcularAuto(costoOriginal,transmision){
@@ -29,10 +31,31 @@ const costoOriginal = parseFloat(costo) || 0;
 const { precioBase, impuesto, granTotal } = calcularAuto(costoOriginal, transmision);
 const formato = (n) => `$${n.toFixed(2)}`;
 
+function calcularCredito(precioBase, formaPago, salario){
+  let capitalFinal = precioBase;
+  
+  if(formaPago === 'credito'){
+    capitalFinal = precioBase * Math.pow(1 + 0.08, 9); // 8% anual, 9 años
+  }
+  
+  capitalFinal *= 1.07; // agregar ITBM
+  
+  const letraMensual = capitalFinal / (9*12);
+  const aprobado = letraMensual <= salario*0.3 ? 'APROBADO' : 'NO APROBADO';
+  
+
+  
+  return {capitalFinal, letraMensual, aprobado};
+}
+
+const {capitalFinal, letraMensual, aprobado} = calcularCredito(precioBase, formaPago, parseFloat(salario) || 0);
+
   return (
     <View style={styles.container}>
      <Text style={styles.titulo}>Venta de Autos</Text>
      <Text style={styles.label}>Costo:</Text>
+
+     {/*input de costos*/}
      <TextInput style={styles.input}
      placeholder='Ingrese el costo. ej:15000'
      keyboardType='numeric'
@@ -40,6 +63,15 @@ const formato = (n) => `$${n.toFixed(2)}`;
      onChangeText={setCosto}
      ></TextInput>
 
+     {/*input de salarios*/}
+     <TextInput style={styles.input}
+     placeholder='Ingrese el salario'
+     keyboardType='numeric'
+     value={salario}
+     onChangeText={setSalario}
+     ></TextInput>
+
+    {/*Radio Button Manual*/}
      <Text style={styles.label}>Transmisión</Text>
      <TouchableOpacity style={styles.radioOption} onPress={()=> setTransmision('manual')}>
       <View style={styles.radioCircle}>
@@ -48,12 +80,36 @@ const formato = (n) => `$${n.toFixed(2)}`;
       <Text>Manual</Text>
      </TouchableOpacity>
 
+    {/*Radio Button automatico*/}
      <TouchableOpacity style={styles.radioOption} onPress={() => setTransmision('automatica')}>
      <View style={styles.radioCircle}>
      {transmision === 'automatica' && <View style={styles.radioDot} />}
      </View>
      <Text>Automática</Text>
-      </TouchableOpacity>
+    </TouchableOpacity>
+
+    {/*Forma de Pago*/}
+    <Text>Forma de Pago</Text>
+    <TouchableOpacity onPress={() => setFormaPago('credito')} style={styles.radioOption}>
+      <View style={styles.radioCircle}>
+        {formaPago === 'credito' && <View style={styles.radioDot} />}
+      </View>
+      <Text>Crédito</Text>
+    </TouchableOpacity>
+
+
+    <TouchableOpacity onPress={() => setFormaPago('contado')} style={styles.radioOption}>
+      <View style={styles.radioCircle}>
+        {formaPago === 'contado' && <View style={styles.radioDot} />}
+      </View>
+      <Text>Contado</Text>
+    </TouchableOpacity>
+
+    
+    <Text>30% del salario: {formato(salario * 0.3)}</Text>
+    <Text>Letra mensual: {formato(letraMensual)}</Text>
+    <Text>Estado crédito: {aprobado}</Text>
+
 
      {costoOriginal > 0 && (
      <View style={styles.resultado}>
@@ -63,6 +119,7 @@ const formato = (n) => `$${n.toFixed(2)}`;
     </View>
       )}
     </View>
+    
   );
 }
 
